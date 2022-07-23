53f1df62fb5621162e1bde39d8ca5349d52a184d
Tushar Nasery <tusharnasery85@gmail.com>
2022-07-23 16:04:53 +0530

    edits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import cardImage from "./logo192.png";
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import CardActions from "@material-ui/core/CardActions";
 
 function App() {
   return (
