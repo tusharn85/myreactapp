89fdb78e91c2953e605034496f213d507c002928
Tushar Nasery <tusharnasery85@gmail.com>
2022-07-24 18:55:16 +0530

    added features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div style={{}}>
       <section className='card'>
-        <img className='card-image' src={cardImage}></img>
-        <h5 className='card-title'>React</h5>
+        <img className='card-image' src="https://www.bing.com/th?id=ORMS.52fe7678cd874531616c3ee65133840b&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1&p=0" alt="News"></img>
+        <a href="https://www.msn.com/en-in/news/other/presidential-candidate-yashwant-sinha-does-not-want-to-return-to-tmc-says-mamata-banerjee-told/ar-AAZUPAN?ocid=msedgntp&cvid=4f700d7f8711488285e86cbf4b96082f"><h5 className='card-title'>Presidential candidate Yashwant Sinha does not want to return to TMC, says Mamata Banerjee TOLD...</h5></a>
       </section>
     </div>
   );
