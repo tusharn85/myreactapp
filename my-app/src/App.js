089bbb59bfb10264237a0a31d9cefb401a9ec203
Tushar Nasery <tusharnasery85@gmail.com>
2022-08-09 23:38:18 +0530

    routes added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
 import NewsApp from './components/NewsApp';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <header>
-      <nav>
-        <ul>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>About</a></li>
-          <li><a href='#'>Contact</a></li>
-        </ul>
-      </nav>
-      </header>
-      <NewsApp />
-    </div>
+    <Router>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><a href='https://www.msn.com/en-in/news/' target="_blank" rel="noreferrer">More News</a></li>
+              <li className='align-right'><Link to='/login'>Login</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <NewsApp />
+        <Routes>
+          <Route path='/login' component={<Login />}>
+            <Login />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Login() {
+  return (
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><a href='/'>Home</a></li>
+          <li><a href='https://www.msn.com/en-in/news/' target="_blank" rel="noreferrer">More News</a></li>
+          <li><a href='/Login'>Login</a></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+  );
+}
